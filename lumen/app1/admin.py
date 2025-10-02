d93ed73bb00eb5dad374b2ace896c194187740d5
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-10-02 16:34:50 -0300

    cadastrando no admin os models

--- lumen/app1/admin.py
+++ lumen/app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Artigos)
+admin.site.register(Sugestao)
+admin.site.register(Progresso)
+admin.site.register(Progresso_diario)
