ea421165f6630f85b247cf745a885427b65c10be
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-10-13 11:26:44 -0300

    implementando a historia de sugestoes e fazendo a base do html para ela

--- lumen/app1/urls.py
+++ lumen/app1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('sugestao/<int:artigo_id>/', views.sugerir_leitura, name='sugestao'),
 ]
--- lumen/app1/views.py
+++ lumen/app1/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Artigos, Sugestao, Progresso, Progresso_diario
 
 # Create your views here.
+
+def sugerir_leitura(request, artigo_id):
+    artigo_atual_id =artigo_id
+    artigo_atual=Artigos.objects.get(id=artigo_atual_id)
+    categoria=artigo_atual.categoria
+    sugestoes=Artigos.objects.order_by('-data_publicacao').filter(categoria=categoria).exclude(id=artigo_atual_id)[:5]
+    context={'artigo_atual':artigo_atual,'sugestoes':sugestoes}
+    return render(request,'sugestao.html',context)
+
+
