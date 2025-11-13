a2f2d26c2949f1c3196ea1c38f860dd2f540a555
joaoeaa <joaoeduardo.andrade03@gmail.com>
2025-11-12 23:35:31 -0300

    Add menu da home

--- lumen/settings.py
+++ lumen/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'app1/static',
-]
 
 
 # Default primary key field type
